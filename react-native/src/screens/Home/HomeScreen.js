5db92e6048480f9331174bfbdbd627e35c14ef02
Krunal Parmar <kunal.ahmedabad@gmail.com>
2021-05-10 15:01:44 +0530

    Displaying match schedule in mobile

--- react-native/src/screens/Home/HomeScreen.js
+++ react-native/src/screens/Home/HomeScreen.js
@@ -1,13 +1,53 @@
+import { useFocusEffect } from '@react-navigation/core';
 import i18n from 'i18n-js';
-import { Text } from 'native-base';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Text, Card, CardItem, Body } from 'native-base';
+
+import React, { useCallback, useState } from 'react';
+import { StyleSheet, View, FlatList } from 'react-native';
+import { getRunningTournament, getMatches } from '../../api/MatchAPI';
 
 function HomeScreen() {
+  const [tournament, setTournament] = useState('');
+  const [matches, setMatches] = useState([]);
+  const getTournament = () => {
+    getRunningTournament().then(data => {
+      setTournament(`${data[0].tournamentName} - ${data[0].season}`);
+      getMatches(data[0].tournamentId, data[0].season).then(data => {
+        setMatches(data);
+      });
+    });
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      getTournament();
+    }, []),
+  );
+
+  const renderItem = ({ item, index }) => (
+    <Card>
+      <CardItem header>
+        <Text>
+          Match {index + 1} {item.homeTeam} vs {item.visitorTeam}
+        </Text>
+      </CardItem>
+      <CardItem>
+        <Text>
+          {item.venue}-{item.playedDate}
+        </Text>
+      </CardItem>
+      <CardItem footer>
+        <Text>{item.winner}</Text>
+      </CardItem>
+    </Card>
+  );
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}> {i18n.t('::Welcome')}</Text>
-      <Text style={styles.centeredText}> {i18n.t('::LongWelcomeMessage')}</Text>
+      {/* <Text style={styles.title}> {i18n.t('::Welcome')}</Text>
+      <Text style={styles.centeredText}> {i18n.t('::LongWelcomeMessage')}</Text> */}
+      <Text>{tournament}</Text>
+      <FlatList data={matches} renderItem={renderItem} keyExtractor={item => item.id} />
     </View>
   );
 }
